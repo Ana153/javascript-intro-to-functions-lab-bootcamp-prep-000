ef4fb07a498286e34862e5bb19ca293f392e4cf8
ana.volaric95@gmail.com <ana.volaric95@gmail.com>
2018-08-24 07:42:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,14 +14,32 @@ function logWhisper(word){
   console.log(word.toLowerCase());
 }
 
+
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
+
+
 function sayHiToGrandma(word){
   if (word.toLowerCase === word){
     return ("I can\'t hear you!")
   } else if (word.toUpperCase === word){
     return("YES INDEED!")
+  } else {
+    return ("I love you, too.")
   }
 }
 
-sayHiToGrandma(bok);
-sayHiToGrandma(BOK);
+sayHiToGrandma(hello);
+sayHiToGrandma(HELLO);
 sayHiToGrandma("I love you, Grandma.");
